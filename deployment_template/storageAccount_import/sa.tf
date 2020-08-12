a5146ba219b52819df424129acf54b15997e296e
dessygold <dessygold@gmail.com>
2020-08-11 20:36:01 -0400

    changes

--- deployment_template/storageAccount_import/sa.tf
+++ deployment_template/storageAccount_import/sa.tf
@@ -1,6 +1,7 @@
 
 module "storageAccount" {
-  source                   = "git::https://gitlabcssdev.fan.gov/cpmo_azure/deployment_code.git//modules/storageAccount?ref=sa-prototype-azr-201" # ref=<release_version>
+  source                   = "git::https://github.com/dessygold/Repos_Acuity_Demo/tree/sa-prototype-azr-201/modules/storageAccount?ref=sa-prototype-azr-20"# ref=<release_version>
+  # source                   = "git::https://gitlabcssdev.fan.gov/cpmo_azure/deployment_code.git//modules/storageAccount?ref=sa-prototype-azr-201" # ref=<release_version>
   resource_group_name      = data.azurerm_resource_group.core-rg.name
   location                 = data.azurerm_resource_group.core-rg.location
   sa_prefix                = var.sa_prefix 
