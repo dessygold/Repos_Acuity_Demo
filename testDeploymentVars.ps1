21dc1c7efc33f0a7476780315e15a76d2023e3b7
dessygold <dessygold@gmail.com>
2020-08-11 22:55:19 -0400

    changes

--- testDeploymentVars.ps1
+++ testDeploymentVars.ps1
@@ -5,15 +5,15 @@ $environment = "AzureCloud" # Get-AzureRmEnvironment | Select-Object Name
 
 # Mgmt variables used to access Service Principle and Keyvault Secret
 $mgmt_subscription_Id = '625f0f3f-397f-43fd-ab0c-53ae2dc9e9d0' # CPMO Temp Tenant
-$mgmt_projectPrefix = 'se-gold'
-$mgmt_sa_prefix = 'segold'
-$mgmt_envIdentifier = "gold_test"
+$mgmt_projectPrefix = 'segold'
+$mgmt_sa_prefix = 'storagegold'
+$mgmt_envIdentifier = "goldenv"
 
 # Subscription deployment specific variables
 $subscription_Id = '625f0f3f-397f-43fd-ab0c-53ae2dc9e9d0' # CPMO Temp Tenant
-$projectPrefix = 'se-gold'
-$sa_prefix = 'segold'
-$envIdentifier = "gold_test"
+$projectPrefix = 'segold'
+$sa_prefix = 'storagegold'
+$envIdentifier = "goldenv"
 $regionSuffix = "eus1"
 $azRegion = 'eastus'
 
